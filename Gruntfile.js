ac67a0c2cd54f4a588976d8ce4e8f7f18c7d2dd5
Scott Andrechek <scottandrechek@gmail.com>
2015-11-26 18:41:59 -0500

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        seperator: ',',
+      },
+      dist: {
+        src: ['src/*.js'],
+        dest: 'dist/debug.js',
+      },
+    },
+    watch: {
+      files: ['src/*.js'],
+      tasks: ['lint', 'concat'],
+    },
+    jshint: {
+      src: ['Gruntfile.js'],
+      beforeconcat: ['src/*.js'],
+      afterconcat: ['dist/debug.js'],
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'dist/release.js': ['src/*.js']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  // Load the plugin that provides the "watch" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'jshint', 'uglify']);
+  // Non-default task(s)
+  grunt.registerTask('wat', ['watch']);
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('cat', ['concat']);
+  grunt.registerTask('ugly', ['uglify']);
+};
