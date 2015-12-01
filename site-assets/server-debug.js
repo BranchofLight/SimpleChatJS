8623027a8b77193bd82f1c8930c89d2a6cb4e425
Scott Andrechek <scottandrechek@gmail.com>
2015-11-30 21:48:13 -0500

    update

--- site-assets/server-debug.js
+++ site-assets/server-debug.js
@@ -141,8 +141,8 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log("Listening on :3000");
+http.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on " + (process.env.PORT || 3000));
 });
 
 /**
